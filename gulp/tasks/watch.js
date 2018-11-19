d7faa22d00b3ebd00084c64ff92720abc3e7624a
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-11-19 18:46:14 +0100

    Migrato a Gulp 4 l'utilizzo di watch e start

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,53 +1,31 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create();
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', function () {
+gulp.task('watch', function(){
 
-    browserSync.init({
-        notify: false, //hide notification label on the browser about browserSync actions
+    browserSync.init ({
+        notify: false,//hide notification label on the browser about browserSync actions
         server: {
             baseDir: "app"
         }
     });
 
-    watch('./app/index.html', function () {
+    gulp.watch('./app/index.html', function(){
         browserSync.reload();
     });
 
-    watch('./app/assets/styles/**/*.css', function () {
-        gulp.start('cssInject');
-    });
-
-    watch('./app/assets/scripts/**/*.js', function () {
-        gulp.start('scriptsRefresh');
-    });
+    gulp.watch('./app/assets/styles/**/*.css', gulp.parallel(gulp.series('styles', 'cssInject')));
+    
+    gulp.watch('./app/assets/scripts/**/*.js', gulp.parallel(gulp.series('scripts', 'scriptsRefresh')));
 
 });
 
-gulp.task('cssInject', gulp.series('styles'), function () { //gulp.series: dependencies that are run in order before the main task
+gulp.task('cssInject', function(){
     return gulp.src('./app/temp/styles/styles.css')
         .pipe(browserSync.stream());
 });
 
-gulp.task('scriptsRefresh', gulp.series('scripts'), function () { //gulp.series: dependencies that are run in order before the main task
+gulp.task('scriptsRefresh', function(){
     browserSync.reload();
 });
-
-/* Gulp 4 dependencies/task execution example
-gulp.task('default',
-    gulp.series('clean', gulp.parallel('scripts', 'styles'),
-        function () {
-            alert("fdgdf");
-        })
-);
-*/
-/* Gulp 3 dependencies/task execution
-gulp.task('scriptsRefresh', ['scripts'], function () {//[dependencies that are run in order before the main task]
-    browserSync.reload();
-});
-gulp.task('cssInject', ['styles'], function () {
-    return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
-});
-*/
